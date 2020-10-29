16e053232bbf4d8d7e8179951965213556ae0e1a
LindseyM20 <lindsey.bordner@gmail.com>
2020-10-28 20:41:30 -0500

    file structure set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('PB Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blucy', false);
---
+++ models/burger.js
@@ -0,0 +1,5 @@
+const orm = require("../config/orm");
+
+// Create the code that will call the ORM functions using burger specific input for the ORM.
+
+// module.exports = ?;
