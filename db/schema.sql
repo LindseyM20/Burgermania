b4a5c168a0e847837872863235cdb079c39d0a6a
LindseyM20 <lindsey.bordner@gmail.com>
2020-10-30 01:27:54 -0500

    adds style.css, JAWSDB code

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,14 @@
 ### Schema
 
 CREATE DATABASE burgers_db;
-
 USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(50) NOT NULL,
-  devoured BOOLEAN,
+	burger_name varchar(100) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
